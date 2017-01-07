f7fee9a2cbeb270d58e0e0dcd1a27e6d97b8dad7
Ashzigur <sanfan@abv.bg>
2017-01-07 13:08:19 +0200

    CHAID initial class structure

---
+++ catdtree/CHAID.py
@@ -0,0 +1,13 @@
+from sklearn.base import BaseEstimator
+
+
+class CHAID(BaseEstimator):
+
+    def __init__(self):
+        pass
+
+    def fit(X, y):
+        pass
+
+    def predict(X):
+        pass
